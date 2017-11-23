f651d5ed6ef79e289168a4eb4b52f4d8396bbb16
Eysteinn Gunnlaugsson <eysteinngu@gmail.com>
2017-11-23 19:02:18 +0100

    woop woop

--- factoring.cpp
+++ factoring.cpp
@@ -36,7 +36,7 @@ int is_prime(mpz_class N, int reps);
 void print_factor(double N);
 
 mpz_class g(mpz_class X, mpz_class N);
-void do_pollard(mpz_class N);
+bool do_pollard(mpz_class N, vector<string> &factors);
 mpz_class pollard(mpz_class N);
 
 int main() {
@@ -45,7 +45,7 @@ int main() {
     int is_prob_prime;
     bool prime_rest;
     vector<string> primes;
-    while(mpz_cmp_si(prime.get_mpz_t(), 50) < 1) {
+    while(mpz_cmp_si(prime.get_mpz_t(), 60000) < 1) {
         mpz_nextprime(prime.get_mpz_t(), prime.get_mpz_t());
         primes.push_back(prime.get_str());
     }
@@ -79,11 +79,14 @@ int main() {
             }
             mpz_nextprime(prime.get_mpz_t(), prime.get_mpz_t());
         }
-        for (auto i = factors.begin(); i != factors.end(); ++i) {
-            cout << *i << endl;
+        if(do_pollard(N, factors)){
+            for (auto i = factors.begin(); i != factors.end(); ++i) {
+                cout << *i << endl;
+            }
+        } else {
+            cout << "fail" << endl;
         }
         factors.clear();
-        do_pollard(N);
         cout << endl;
     }
     return 0;
@@ -107,19 +110,23 @@ int is_prime(mpz_class N, int reps){
 mpz_class g(mpz_class X, mpz_class N) {
     mpz_class rop;
     mpz_pow_ui(rop.get_mpz_t(), X.get_mpz_t(), 2);
-    rop += rand() % 2321 + 47;
-//    rop++;
+//    rop += rand() % 2321 + 47;
+    rop++;
     mpz_mod(rop.get_mpz_t(), rop.get_mpz_t(), N.get_mpz_t());
     return rop;
 }
 
 mpz_class pollard(mpz_class N){
     mpz_class x = 2, x_fixed = 2, size = 2, factor = 1, mathz;
+    int counter = 0;
     while(factor == 1) {
         for(size_t c = 1; c <= size && factor <= 1; c++) {
             x = g(x, N);
             mathz = x - x_fixed;
             mpz_gcd(factor.get_mpz_t(), mathz.get_mpz_t(), N.get_mpz_t());
+            if(++counter >= 300000){
+                return -1;
+            }
         }
         size *= 2;
         x_fixed = x;
@@ -127,21 +134,21 @@ mpz_class pollard(mpz_class N){
     return factor;
 }
 
-void do_pollard(mpz_class N){
-    int counter = 0;
+bool do_pollard(mpz_class N, vector<string> &factors){
+    mpz_class factor;
     while(true){
-        if(++counter == 100000){
-            throw;
-        }
-        mpz_class factor;
         if(is_prime(N, 50)){
-            cout << N << endl;
+            factors.push_back(N.get_str());
             break;
         }
         factor = pollard(N);
-        cout << factor << endl;
+        if(factor == -1){
+            return false;
+        }
+        factors.push_back(factor.get_str());
         N /= factor;
     }
+    return true;
 }
 
 void print_factor(double N) {
