aeb029ac826f562d7c68e13c660b265b2dc96ae6
Eysteinn Gunnlaugsson <eysteinngu@gmail.com>
2017-11-23 15:14:02 +0100

    working GCD for kattis..

--- factoring.cpp
+++ factoring.cpp
@@ -25,28 +25,29 @@ using namespace std;
 
 #define all(o) (o).begin(), (o).end()
 #define allr(o) (o).rbegin(), (o).rend()
-const int INF = 2147483647;
 typedef long long ll;
 typedef unsigned long long ull;
 template <class T> int size(T &x) { return x.size(); }
 
 void factor(double N);
 
-int is_prime(mpz_t N, int reps);
+int is_prime(mpz_class N, int reps);
 
 void print_factor(double N);
 
 double g(double X, double N);
 
 int main() {
-    mpz_t N;
-    int is_prob_prime;
+    mpz_class N;
+    mpz_class GCD;
+    int is_prob_prime = 0;
 
     while(gmp_scanf("%Zd", &N) != EOF) {
         is_prob_prime = is_prime(N, 50);
         if(is_prob_prime == 2 || is_prob_prime == 1){
             cout << N << endl << endl;
         } else {
+            mpz_gcd(GCD.get_mpz_t(), N.get_mpz_t(), N.get_mpz_t());
             cout << "fail" << endl << endl;
         }
     }
@@ -55,12 +56,12 @@ int main() {
 
 /// Estimates if N is prime. It is not guaranteed t be correct. Higher reps mean higher likelihood of being correct.
 /// \param N Number to check
-/// \param reps Number of times the algorithm chekcs if N is prime
+/// \param reps Number of times the algorithm checks if N is prime
 /// \return 0 if not Prime!
 ///         1 if maybe Prime!
 ///         2 if definitely Prime!
-int is_prime(mpz_t N, int reps){
-    return mpz_probab_prime_p(N, reps);
+int is_prime(mpz_class N, int reps){
+    return mpz_probab_prime_p(N.get_mpz_t(), reps);
 }
 
 
