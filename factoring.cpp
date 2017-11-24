d6ee18c341afab2ca8b6484cc05fc5310190b9c3
Eysteinn Gunnlaugsson <eysteinngu@gmail.com>
2017-11-24 17:53:00 +0100

    score 77.

--- factoring.cpp
+++ factoring.cpp
@@ -31,13 +31,11 @@ template <class T> int size(T &x) { return x.size(); }
 
 void factor(double N);
 
-int is_prime(mpz_class N, int reps);
+int is_prime(mpz_class &N, int reps);
 
-void print_factor(double N);
-
-mpz_class g(mpz_class X, mpz_class N);
-bool do_pollard(mpz_class N, vector<string> &factors);
-mpz_class pollard(mpz_class N);
+//mpz_class g(mpz_class &X, mpz_class &N);
+bool do_pollard(mpz_class &N, vector<string> &factors);
+mpz_class pollard(mpz_class &N);
 
 int main() {
     mpz_class polli;
@@ -98,55 +96,71 @@ int main() {
 /// \return 0 if not Prime!
 ///         1 if maybe Prime!
 ///         2 if definitely Prime!
-int is_prime(mpz_class N, int reps){
+int is_prime(mpz_class &N, int reps){
     return mpz_probab_prime_p(N.get_mpz_t(), reps);
 }
 
 
-/// This is the g function. If you dont know the g you ain't no g...g
+/// Having this in a function makes the code much slower
 /// \param X
 /// \param N
 /// \return
-mpz_class g(mpz_class X, mpz_class N) {
-    mpz_class rop;
-    mpz_pow_ui(rop.get_mpz_t(), X.get_mpz_t(), 2);
-//    rop += rand() % 2321 + 47;
-    rop++;
-    mpz_mod(rop.get_mpz_t(), rop.get_mpz_t(), N.get_mpz_t());
-    return rop;
-}
+//mpz_class g(mpz_class &X, mpz_class &N) {
+//    mpz_class rop;
+//    mpz_pow_ui(rop.get_mpz_t(), X.get_mpz_t(), 2);
+//    mpz_add_ui(rop.get_mpz_t(), rop.get_mpz_t(), 1);
+//    mpz_mod(rop.get_mpz_t(), rop.get_mpz_t(), N.get_mpz_t());
+//    return rop;
+//}
 
-mpz_class pollard(mpz_class N){
-    mpz_class x = 2, x_fixed = 2, size = 2, factor = 1, mathz;
-    int counter = 0;
-    while(factor == 1) {
-        for(size_t c = 1; c <= size && factor <= 1; c++) {
-            x = g(x, N);
-            mathz = x - x_fixed;
-            mpz_gcd(factor.get_mpz_t(), mathz.get_mpz_t(), N.get_mpz_t());
-            if(++counter >= 590000){
-                return -1;
-            }
-        }
-        size *= 2;
-        x_fixed = x;
-    }
-    return factor;
-}
+//mpz_class pollard(mpz_class &N){
+//    mpz_class x = 2, x_fixed = 2, size = 2, factor = 1, mathz;
+//    int counter = 0;
+//    while(factor == 1) {
+////        for(size_t c = 1; c <= size && factor <= 1; c++) {
+//        for(size_t c = 1; c <= size && factor <= 1; c++) {
+//            mpz_pow_ui(x.get_mpz_t(), x.get_mpz_t(), 2);
+//            mpz_add_ui(x.get_mpz_t(), x.get_mpz_t(), 1);
+//            mpz_mod(x.get_mpz_t(), x.get_mpz_t(), N.get_mpz_t());
+//            mathz = x - x_fixed;
+//            mpz_gcd(factor.get_mpz_t(), mathz.get_mpz_t(), N.get_mpz_t());
+//            if(++counter >= 1000000){
+//                return -1;
+//            }
+//        }
+//        size *= 2;
+//        x_fixed = x;
+//    }
+//    return factor;
+//}
 
-bool do_pollard(mpz_class N, vector<string> &factors){
-    mpz_class factor;
+bool do_pollard(mpz_class &N, vector<string> &factors){
+    int counter;
+    mpz_class x, size, factor, mathz, y;
     while(true){
-        if(is_prime(N, 15)){
+        if(mpz_probab_prime_p(N.get_mpz_t(), 15)){
             factors.push_back(N.get_str());
             break;
         }
-        factor = pollard(N);
+        counter = 0;
+        x = 2, size = 2, factor = 1, y = 2;
+        for(;factor == 1; size*=2) {
+            for(size_t c = 1; c <= size && factor <= 1; ++c) {
+                x = ((x*x)+1) % N;
+                y = ((y*y)+1) % N;
+                y = ((y*y)+1) % N;
+                mathz = (x - y) % N;
+                mpz_gcd(factor.get_mpz_t(), mathz.get_mpz_t(), N.get_mpz_t());
+                if(++counter >= 700000){
+                    return false;
+                }
+            }
+        }
         if(factor == -1){
             return false;
         }
         factors.push_back(factor.get_str());
-        N /= factor;
+        mpz_divexact(N.get_mpz_t(), N.get_mpz_t(), factor.get_mpz_t());
     }
     return true;
 }
