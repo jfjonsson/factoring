77fa35fa5c364f3729bb246252b91b50701c03ac
Eysteinn Gunnlaugsson <eysteinngu@gmail.com>
2017-11-27 14:35:37 +0100

    little clean up

--- factoring.cpp
+++ factoring.cpp
@@ -1,9 +1,6 @@
 #include <algorithm>
-#include <time.h>
 #include <gmp.h>
 #include <gmpxx.h>
-#include <bitset>
-#include <cassert>
 #include <cmath>
 #include <cstdio>
 #include <cstdlib>
@@ -12,32 +9,12 @@
 #include <fstream>
 #include <iomanip>
 #include <iostream>
-#include <list>
-#include <map>
-#include <queue>
-#include <set>
-#include <unordered_set>
-#include <sstream>
-#include <stack>
 #include <string>
-#include <utility>
 #include <vector>
 
 using namespace std;
 
-#define all(o) (o).begin(), (o).end()
-#define allr(o) (o).rbegin(), (o).rend()
-typedef long long ll;
-typedef unsigned long long ull;
-template <class T> int size(T &x) { return x.size(); }
-
-void factor(double N);
-
-int is_prime(mpz_class &N, int reps);
-
-//mpz_class g(mpz_class &X, mpz_class &N);
 bool do_pollard(mpz_class &N, vector<string> &factors);
-mpz_class pollard(mpz_class &N);
 
 int main() {
     mpz_class polli;
@@ -52,7 +29,7 @@ int main() {
     vector<string> factors;
     while(gmp_scanf("%Zd", &N) != EOF) {
         prime_rest = false;
-        is_prob_prime = is_prime(N, 15);
+        is_prob_prime = mpz_probab_prime_p(N.get_mpz_t(), 15);
         if(is_prob_prime){
             cout << N << endl << endl;
             continue;
@@ -66,7 +43,7 @@ int main() {
                     mpz_divexact (N.get_mpz_t(), N.get_mpz_t(), prime.get_mpz_t());
                     mpz_mod(remainder.get_mpz_t(), N.get_mpz_t(), prime.get_mpz_t());
                 }
-                is_prob_prime = is_prime(N, 15);
+                is_prob_prime = mpz_probab_prime_p(N.get_mpz_t(), 15);
                 if(is_prob_prime){
                     prime_rest = true;
                     break;
@@ -89,53 +66,10 @@ int main() {
     return 0;
 }
 
-/// Estimates if N is prime. It is not guaranteed t be correct. Higher reps mean higher likelihood of being correct.
-/// \param N Number to check
-/// \param reps Number of times the algorithm checks if N is prime
-/// \return 0 if not Prime!
-///         1 if maybe Prime!
-///         2 if definitely Prime!
-int is_prime(mpz_class &N, int reps){
-    return mpz_probab_prime_p(N.get_mpz_t(), reps);
-}
-
-
-/// Having this in a function makes the code much slower
-/// \param X
-/// \param N
-/// \return
-//mpz_class g(mpz_class &X, mpz_class &N) {
-//    mpz_class rop;
-//    mpz_pow_ui(rop.get_mpz_t(), X.get_mpz_t(), 2);
-//    mpz_add_ui(rop.get_mpz_t(), rop.get_mpz_t(), 1);
-//    mpz_mod(rop.get_mpz_t(), rop.get_mpz_t(), N.get_mpz_t());
-//    return rop;
-//}
-
-//mpz_class pollard(mpz_class &N){
-//    mpz_class x = 2, x_fixed = 2, size = 2, factor = 1, mathz;
-//    int counter = 0;
-//    while(factor == 1) {
-////        for(size_t c = 1; c <= size && factor <= 1; c++) {
-//        for(size_t c = 1; c <= size && factor <= 1; c++) {
-//            mpz_pow_ui(x.get_mpz_t(), x.get_mpz_t(), 2);
-//            mpz_add_ui(x.get_mpz_t(), x.get_mpz_t(), 1);
-//            mpz_mod(x.get_mpz_t(), x.get_mpz_t(), N.get_mpz_t());
-//            mathz = x - x_fixed;
-//            mpz_gcd(factor.get_mpz_t(), mathz.get_mpz_t(), N.get_mpz_t());
-//            if(++counter >= 1000000){
-//                return -1;
-//            }
-//        }
-//        size *= 2;
-//        x_fixed = x;
-//    }
-//    return factor;
-//}
 
 bool do_pollard(mpz_class &N, vector<string> &factors){
     srand(time(NULL));
-    long counter, bent_counter = 0, start = 12459026053, bent_max = 50;
+    long counter, bent_counter = 0, start = 12459026053, bent_max = 40;
     mpz_class x, factor, mathz, y, bent_product = 1;
     while(true){
         if(mpz_probab_prime_p(N.get_mpz_t(), 15)){
@@ -153,12 +87,12 @@ bool do_pollard(mpz_class &N, vector<string> &factors){
             y = ((y*y)+1) % N;
             y = ((y*y)+1) % N;
             bent_product *= (x > y ? x - y : y - x);
-            if (bent_counter++ == bent_max) {
+            if (counter % bent_max == 0) {
                 mpz_gcd(factor.get_mpz_t(), bent_product.get_mpz_t(), N.get_mpz_t());
                 bent_counter = 0;
                 bent_product = 1;
             }
-            if(++counter >= 500000){
+            if(++counter >= 555000){
                 return false;
             }
             if (N == factor) {
@@ -170,7 +104,7 @@ bool do_pollard(mpz_class &N, vector<string> &factors){
         }
         mpz_divexact(N.get_mpz_t(), N.get_mpz_t(), factor.get_mpz_t());
         if (mpz_probab_prime_p(factor.get_mpz_t(), 15)) {
-            factors.push_back(factor.get_str()); 
+            factors.push_back(factor.get_str());
         } else {
             bool res = do_pollard(factor, factors);
             if (!res) {
