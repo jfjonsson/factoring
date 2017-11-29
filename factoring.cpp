67a209666146f3b6719ae5f5e6442118ebf33148
Eysteinn Gunnlaugsson <eysteinngu@gmail.com>
2017-11-29 15:44:01 +0100

    ready for zippa og skila

--- factoring.cpp
+++ factoring.cpp
@@ -1,59 +1,87 @@
-#include <algorithm>
 #include <gmp.h>
 #include <gmpxx.h>
-#include <cmath>
-#include <cstdio>
-#include <cstdlib>
-#include <cstring>
-#include <ctime>
-#include <fstream>
-#include <iomanip>
 #include <iostream>
 #include <string>
 #include <vector>
 
 using namespace std;
 
-bool do_pollard(mpz_class &N, vector<string> &factors);
+// Pollard's rho algorithm implementation
+bool do_pollard(mpz_class &N, vector<string> &factors, long start){
+    int brent_max=35, counter=0;
+    mpz_class x, y, factor, brent_product = 1;
+    while(!mpz_probab_prime_p(N.get_mpz_t(), 15)){
+        counter = 0;
+        x = y = start;
+        factor = 1;
+        while (factor == 1) {
+            if(++counter >= 650000){ return false; }
+            x = (x * x + 1) % N;
+            x = (x * x + 1) % N;
+            y = (y * y + 1) % N;
+            y = (y * y + 1) % N;
+            y = (y * y + 1) % N;
+            y = (y * y + 1) % N;
+            brent_product *= x - y;
+            // We use Brent-Pollard cycle detection to save us from doing multiple GCD calculations
+            if (counter % brent_max == 0) {
+                mpz_gcd(factor.get_mpz_t(), brent_product.get_mpz_t(), N.get_mpz_t());
+                brent_product = 1;
+            }
+            if (N == factor) {
+                factor = 1;
+            }
+        }
+        N /= factor;
+        // If the factor is a prime number continue running Pollard's rho with the remainder
+        // else we recursively factor that factor.
+        if (mpz_probab_prime_p(factor.get_mpz_t(), 15)) {
+            factors.push_back(factor.get_str());
+        } else {
+            if (!do_pollard(factor, factors, start)) {
+                return false;
+            }
+        }
+    }
+    factors.push_back(N.get_str());
+    return true;
+}
 
 int main() {
-    mpz_class N, prime, remainder;
-    int is_prob_prime;
-    bool prime_rest;
-    vector<string> primes;
-    primes.reserve(3000);
-    while(mpz_cmp_si(prime.get_mpz_t(), 3000) < 1) {
+    size_t i, prime_threshold = 3000, start=12459026053;
+    mpz_class N, prime;
+    vector<string> primes(430);  // number of primes from 0 to 3000
+    // Pre compute all primes under 3000
+    while(prime < prime_threshold){
         mpz_nextprime(prime.get_mpz_t(), prime.get_mpz_t());
         primes.push_back(prime.get_str());
     }
     vector<string> factors;
     while(gmp_scanf("%Zd", &N) != EOF) {
-        prime_rest = false;
-        is_prob_prime = mpz_probab_prime_p(N.get_mpz_t(), 15);
-        if(is_prob_prime){
+        // If N is prime print it and move on to the next N
+        if(mpz_probab_prime_p(N.get_mpz_t(), 15)){
             cout << N << endl << endl;
             continue;
         }
-        for (unsigned int i=0; i < primes.size(); i++) {
+        // Check if the first 3000 primes are factors in N
+        for (i=0; i < primes.size(); ++i) {
             mpz_set_str(prime.get_mpz_t(), primes[i].c_str(), 10);
-            mpz_mod(remainder.get_mpz_t(), N.get_mpz_t(), prime.get_mpz_t());
-            if (mpz_cmp_si(remainder.get_mpz_t(), 0) == 0) {
-                while(mpz_cmp_si(remainder.get_mpz_t(), 0) == 0) {
-                    factors.push_back(prime.get_str());
-                    mpz_divexact (N.get_mpz_t(), N.get_mpz_t(), prime.get_mpz_t());
-                    mpz_mod(remainder.get_mpz_t(), N.get_mpz_t(), prime.get_mpz_t());
-                }
-                is_prob_prime = mpz_probab_prime_p(N.get_mpz_t(), 15);
-                if(is_prob_prime){
-                    prime_rest = true;
-                    break;
+            if(N % prime == 0){
+                if(!mpz_probab_prime_p(N.get_mpz_t(), 15)){
+                    // if we find a factor check if it is a repeated factor in N
+                    while(N % prime == 0){
+                        factors.push_back(prime.get_str());
+                        N /= prime;
+                    }
                 }
             }
-            if (prime_rest) {
+            // If the remainder is prime we break and print out the factors
+            if(mpz_probab_prime_p(N.get_mpz_t(), 15)){
                 break;
             }
         }
-        if(do_pollard(N, factors)){
+        // Run Pollard's rho algorithm to find larger factors
+        if(do_pollard(N, factors, start)){
             for (auto i = factors.begin(); i != factors.end(); ++i) {
                 cout << *i << endl;
             }
@@ -63,46 +91,4 @@ int main() {
         factors.clear();
         cout << endl;
     }
-    return 0;
-}
-
-
-bool do_pollard(mpz_class &N, vector<string> &factors){
-    int bent_max=35, counter=0;
-    long start=12459026053;
-    mpz_class x, y, factor, bent_product = 1;
-    while(!mpz_probab_prime_p(N.get_mpz_t(), 15)){
-        counter = 0;
-        x = y = start;
-        factor = 1;
-        while (factor == 1) {
-            if(++counter >= 555000){ return false; }
-            x = ((x*x)+1) % N;
-            x = ((x*x)+1) % N;
-            y = ((y*y)+1) % N;
-            y = ((y*y)+1) % N;
-            y = ((y*y)+1) % N;
-            y = ((y*y)+1) % N;
-            y = ((y*y)+1) % N;
-            bent_product *= (x > y ? x - y : y - x);
-            if (counter % bent_max == 0) {
-                mpz_gcd(factor.get_mpz_t(), bent_product.get_mpz_t(), N.get_mpz_t());
-                bent_product = 1;
-            }
-            if (N == factor) {
-                factor = 1;
-                x = y = ++start;
-            }
-        }
-        mpz_divexact(N.get_mpz_t(), N.get_mpz_t(), factor.get_mpz_t());
-        if (mpz_probab_prime_p(factor.get_mpz_t(), 15)) {
-            factors.push_back(factor.get_str());
-        } else {
-            if (!do_pollard(factor, factors)) {
-                return false;
-            }
-        }
-    }
-    factors.push_back(N.get_str());
-    return true;
 }
